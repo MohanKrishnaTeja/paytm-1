82e405d0e54f333f0499f5445836f741de5f02e2
KRSHNA <mohankrishnateja2022@gmail.com>
2024-05-03 11:29:47 +0530

    changed port

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const mainRouter = require("./routes/index");
 const cors = require("cors");
 const app = express();
+const port = 8000;
 app.use(cors);
 app.use(express.json());
 
 app.use("api/v1",mainRouter);
 
-app.listen(8000);
+app.listen(port, () => {
+    console.log(` listening on port ${port}`)
+  })
