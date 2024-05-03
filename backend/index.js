bcd78672ce3f4b3be9e33860b1a4764452a650d9
KRSHNA <mohankrishnateja2022@gmail.com>
2024-05-03 12:41:23 +0530

    started frontend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,29 @@
-const express = require("express");
-const mainRouter = require("./routes/index");
-const cors = require("cors");
+// const express = require("express");
+// const mainRouter = require("./routes/index");
+// const cors = require("cors");
+// const app = express();
+// const port = 8000;
+// app.use(cors);
+// app.use(express.json());
+
+// app.use("api/v1",mainRouter);
+
+// app.listen(port, () => {
+//     console.log(` listening on port ${port}`)
+//   })
+
+
+const express = require('express');
+const cors = require('cors');
+const mainRouter = require('./routes/index')
 const app = express();
-const port = 8000;
-app.use(cors);
-app.use(express.json());
 
-app.use("api/v1",mainRouter);
+app.use(express.json())
+app.use(cors())
 
-app.listen(port, () => {
-    console.log(` listening on port ${port}`)
-  })
+app.use('/api/v1',mainRouter);
+
+let PORT =  3900
+app.listen(PORT,(req,res)=>{
+    console.log(`Listening to port ${PORT}`);
+})
